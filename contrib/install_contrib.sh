79a0aa42f41ae2c540228e1a07fa28cc2607cc06
zsxwing <zsxwing@gmail.com>
2012-12-21 18:58:58 +0800

    Create lib when need

--- contrib/install_contrib.sh
+++ contrib/install_contrib.sh
@@ -4,5 +4,6 @@ cd $( dirname $0 ) && rm -rf gtest-1.6.0 && unzip gtest-1.6.0.zip || exit $?
 g++ -Igtest-1.6.0/include -Igtest-1.6.0 -c gtest-1.6.0/src/gtest-all.cc gtest-1.6.0/src/gtest_main.cc || exit $?
 ar -rv libgtest.a gtest-all.o gtest_main.o || exit $?
 rm -rf gtest-all.o gtest_main.o || exit $?
+mkdir ../lib/
 mv libgtest.a ../lib/ || exit $?
 

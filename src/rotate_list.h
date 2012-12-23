d8a063f1d749b597ebe12758a8f413e3f7588bb7
zsxwing <zsxwing@gmail.com>
2012-12-23 21:57:40 +0800

    Use k % listSize to reduce the repeated search

--- src/rotate_list.h
+++ src/rotate_list.h
@@ -5,11 +5,22 @@ public:
       return head;
     }
     ListNode *tail = head;
+    int listSize = 0;
     while (k > 0) {
       k--;
+      listSize++;
       tail = tail->next;
       if (tail == NULL) {
+        // Here we get the list size.
+        // We can use k % listSize to reduce the repeated search.
         tail = head;
+        k = k % listSize;
+        while (k > 0) {
+          k--;
+          // We do not need to check if tail is NULL, since 0 <= k < listSize.
+          tail = tail->next;
+        }
+        break;
       }
     }
     ListNode *rotate = head;

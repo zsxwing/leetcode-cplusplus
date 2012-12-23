bd4bffad9ff5b33b1d5c72956689b4f95ded6f03
zsxwing <zsxwing@gmail.com>
2012-12-23 18:25:09 +0800

    Format

--- test/interleaving_string_test.cc
+++ test/interleaving_string_test.cc
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "interleaving_string.h"
 #include <gtest/gtest.h>
+
 TEST(InterleavingString, isInterleave) {
   Solution s;
   ASSERT_TRUE(s.isInterleave("aabcc","dbbca","aadbbcbcac"));

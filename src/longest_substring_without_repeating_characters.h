89166571aaf374980852f00a1050f1eed989e68e
zsxwing <zsxwing@gmail.com>
2013-09-15 16:36:38 +0800

    Solve the 'Longest Substring Without Repeating Characters' question

--- src/longest_substring_without_repeating_characters.h
+++ src/longest_substring_without_repeating_characters.h
@@ -1,32 +1,15 @@
-#include <string>
-#include <algorithm>
-using namespace std;
-
 class Solution {
-private:
-  const char *searchTheEndChar(const char *p, const char *end) {
-    char c = *end;
-    while (p < end && *p != c) {
-      p++;
-    }
-    return p == end ? NULL : p;
-  }
-
 public:
   int lengthOfLongestSubstring(string s) {
-    const char *p = s.c_str();
-    int max_substring_length = 0;
-    const char* substring_begin = p;
-    while (*p) {
-      const char *repeated_char = searchTheEndChar(substring_begin, p);
-      if (repeated_char) {
-        // We find a repeated char.
-        max_substring_length = max(max_substring_length,
-            (int) (p - substring_begin));
-        substring_begin = repeated_char + 1;
-      }
-      p++;
+    int lastPos[CHAR_MAX + 1];
+    fill(lastPos, lastPos + (CHAR_MAX + 1), -1);
+    int maxSubstringLen = 0;
+    int substringBegin = 0;
+    for (int i = 0; i < s.size(); i++) {
+      substringBegin = max(substringBegin, lastPos[s[i]] + 1);
+      lastPos[s[i]] = i;
+      maxSubstringLen = max(maxSubstringLen, i - substringBegin + 1);
     }
-    return max(max_substring_length, (int) (p - substring_begin));
+    return maxSubstringLen;
   }
 };

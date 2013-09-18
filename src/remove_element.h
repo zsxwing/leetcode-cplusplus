9d192245ee076d1d956e9ac38da372ffe6fc8ace
zsxwing <zsxwing@gmail.com>
2013-09-18 19:38:50 +0800

    Solve the 'Remove Element' question

--- src/remove_element.h
+++ src/remove_element.h
@@ -1,14 +1,12 @@
 class Solution {
 public:
   int removeElement(int A[], int n, int elem) {
-    int elemCount = 0;
+    int len = 0;
     for (int i = 0; i < n; i++) {
-      if (A[i] == elem) {
-        elemCount++;
-      } else if (elemCount) {
-        A[i - elemCount] = A[i];
+      if (A[i] != elem) {
+        A[len++] = A[i];
       }
     }
-    return n - elemCount;
+    return len;
   }
 };

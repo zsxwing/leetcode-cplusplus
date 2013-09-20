e942085823392d6497b18ddd994cb028df4e3276
zsxwing <zsxwing@gmail.com>
2013-09-20 22:59:30 +0800

    Solve the 'Plus One' question

--- src/plus_one.h
+++ src/plus_one.h
@@ -1,20 +1,11 @@
-#include <vector>
-using namespace std;
-
 class Solution {
-
 public:
   vector<int> plusOne(vector<int> &digits) {
     int carry = 1;
     for (int i = digits.size() - 1; i >= 0; i--) {
       digits[i] += carry;
-      if (digits[i] >= 10) {
-        digits[i] -= 10;
-        carry = 1;
-      } else {
-        carry = 0;
-        break;
-      }
+      carry = digits[i] / 10;
+      digits[i] %= 10;
     }
     if (carry > 0) {
       digits.insert(digits.begin(), carry);

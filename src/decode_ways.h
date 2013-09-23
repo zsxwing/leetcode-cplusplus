d59ce46ce21edfac7130d1a8226d7db0d387b716
zsxwing <zsxwing@gmail.com>
2013-09-23 21:58:04 +0800

    Solve the 'Decode Ways' question

--- src/decode_ways.h
+++ src/decode_ways.h
@@ -1,37 +1,19 @@
-#include <string>
-using namespace std;
-
 class Solution {
-
-private:
-  int numDecodings(const char *s, int len) {
-    if (*s == '\0' || *s == '0') {
-      return 0;
-    }
-    int* dp = new int[len + 1];
-    dp[0] = 1;
-    dp[1] = 1;
-    int i = 1;
-    while (s[i] != '\0') {
-      if (s[i] != '0') {
-        dp[i + 1] = dp[i];
-      } else {
-        dp[i + 1] = 0;
-      }
-      if (s[i - 1] == '1') {
-        dp[i + 1] += dp[i - 1];
-      } else if (s[i - 1] == '2' && s[i] <= '6') {
-        dp[i + 1] += dp[i - 1];
-      }
-      ++i;
-    }
-    int re = dp[len];
-    delete[] dp;
-    return re;
-  }
-
 public:
   int numDecodings(string s) {
-    return numDecodings(s.c_str(), s.size());
+    if (s.size() == 0) {
+      return 0;
+    }
+    int dp[s.size() + 1];
+    dp[0] = 1;
+    dp[1] = s[0] == '0' ? 0 : 1;
+    int i = 1;
+    for (int i = 1; i < s.size(); i++) {
+      dp[i + 1] = s[i] == '0' ? 0 : dp[i];
+      if (s[i - 1] == '1' || (s[i - 1] == '2' && s[i] <= '6')) {
+        dp[i + 1] += dp[i - 1];
+      }
+    }
+    return dp[s.size()];
   }
 };

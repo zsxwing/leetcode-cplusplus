9718ef838b47e9813df41c57acc93c6d1160cac5
zsxwing <zsxwing@gmail.com>
2013-09-23 22:34:16 +0800

    Solve the 'Partition List' question

--- src/partition_list.h
+++ src/partition_list.h
@@ -1,38 +1,23 @@
 class Solution {
-
 public:
   ListNode *partition(ListNode *head, int x) {
-    ListNode *lessTail = NULL;
     ListNode *lessHead = NULL;
+    ListNode **lessTail = &lessHead;
     ListNode *greaterHead = NULL;
-    ListNode *greaterTail = NULL;
-    ListNode *next = head;
-    while (next) {
-      if (next->val < x) {
-        if (lessTail) {
-          lessTail->next = next;
-        } else {
-          lessHead = next;
-        }
-        lessTail = next;
+    ListNode **greaterTail = &greaterHead;
+    ListNode *p = head;
+    while (p) {
+      if (p->val < x) {
+        *lessTail = p;
+        lessTail = &p->next;
       } else {
-        if (greaterTail) {
-          greaterTail->next = next;
-        } else {
-          greaterHead = next;
-        }
-        greaterTail = next;
+        *greaterTail = p;
+        greaterTail = &p->next;
       }
-      next = next->next;
+      p = p->next;
     }
-    if (lessHead == NULL) {
-      lessHead = greaterHead;
-    } else {
-      lessTail->next = greaterHead;
-    }
-    if (greaterTail) {
-      greaterTail->next = NULL;
-    }
+    *lessTail = greaterHead;
+    *greaterTail = NULL;
     return lessHead;
   }
 };

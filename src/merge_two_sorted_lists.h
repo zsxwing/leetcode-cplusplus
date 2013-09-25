caff5c67989ff171f7c7d704fb97acc255c9d081
zsxwing <zsxwing@gmail.com>
2013-09-25 21:17:24 +0800

    Solve the 'Merge Two Sorted Lists' question

---
+++ src/merge_two_sorted_lists.h
@@ -0,0 +1,25 @@
+class Solution {
+public:
+  ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+    ListNode *head = NULL;
+    ListNode **pre = &head;
+    while (l1 && l2) {
+      if (l1->val > l2->val) {
+        *pre = l2;
+        pre = &(l2->next);
+        l2 = l2->next;
+      } else {
+        *pre = l1;
+        pre = &(l1->next);
+        l1 = l1->next;
+      }
+    }
+    if (l1) {
+      *pre = l1;
+    }
+    if (l2) {
+      *pre = l2;
+    }
+    return head;
+  }
+};
---
+++ test/merge_two_sorted_lists_test.cc
@@ -0,0 +1,4 @@
+#include "common.h"
+#include "merge_two_sorted_lists.h"
+#include <gtest/gtest.h>
+

cd0e1ffebcde1c08e67def81dc54b97e1a2d3f14
zsxwing <zsxwing@gmail.com>
2013-09-25 21:20:03 +0800

    Solve the 'Merge Two Sorted Lists' question

--- src/merge_two_sorted_lists.h
+++ src/merge_two_sorted_lists.h
@@ -6,13 +6,12 @@ public:
     while (l1 && l2) {
       if (l1->val > l2->val) {
         *pre = l2;
-        pre = &(l2->next);
         l2 = l2->next;
       } else {
         *pre = l1;
-        pre = &(l1->next);
         l1 = l1->next;
       }
+      pre = &((*pre)->next);
     }
     if (l1) {
       *pre = l1;

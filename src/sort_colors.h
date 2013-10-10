692851b3957cf87cb1852f900498c549bbcea757
zsxwing <zsxwing@gmail.com>
2013-10-10 21:32:59 +0800

    Solve the 'Sort Colors' question

--- src/sort_colors.h
+++ src/sort_colors.h
@@ -1,38 +1,18 @@
 class Solution {
-
 public:
-  void sortColors(int A[], int n) {
-    int firstOne = -1;
-    int firstTwo = -1;
-    for (int i = 0; i < n; i++) {
-      if (A[i] == 0) {
-        if (firstOne >= 0) {
-          A[firstOne++] = 0;
-          if (firstTwo >= 0) {
-            A[firstTwo++] = 1;
-            A[i] = 2;
-          } else {
-            A[i] = 1;
-          }
-        } else if (firstTwo >= 0) {
-          A[firstTwo++] = 0;
-          A[i] = 2;
+    void sortColors(int A[], int n) {
+        int zero = -1;
+        int one = -1;
+        for(int i = 0; i<n; i++) {
+            if(A[i] == 0) {
+                A[i] = A[++zero];
+                A[zero] = 0;
+                one = max(zero, one);
+            }
+            if(A[i] == 1) {
+                A[i] = A[++one];
+                A[one] = 1;
+            }
         }
-      } else if (A[i] == 1) {
-        if (firstTwo >= 0) {
-          if (firstOne < 0) {
-            firstOne = firstTwo;
-          }
-          A[firstTwo++] = 1;
-          A[i] = 2;
-        } else if (firstOne < 0) {
-          firstOne = i;
-        }
-      } else {
-        if (firstTwo < 0) {
-          firstTwo = i;
-        }
-      }
     }
-  }
 };

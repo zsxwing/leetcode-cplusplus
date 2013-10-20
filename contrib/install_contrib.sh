f8f12787232aba716ff8f0928b4a0377ba1f3a4e
zsxwing <zsxwing@gmail.com>
2013-10-20 15:13:39 +0800

    Updated the gtest script

--- contrib/install_contrib.sh
+++ contrib/install_contrib.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash -e
 
-cd $( dirname $0 ) && rm -rf gtest-1.6.0 && unzip gtest-1.6.0.zip || exit $?
-g++ -Igtest-1.6.0/include -Igtest-1.6.0 -c gtest-1.6.0/src/gtest-all.cc gtest-1.6.0/src/gtest_main.cc || exit $?
-ar -rv libgtest.a gtest-all.o gtest_main.o || exit $?
-rm -rf gtest-all.o gtest_main.o || exit $?
-mkdir ../lib/
-mv libgtest.a ../lib/ || exit $?
+cd $( dirname $0 ) && rm -rf gtest-1.6.0 && unzip gtest-1.6.0.zip
+g++ -Igtest-1.6.0/include -Igtest-1.6.0 -c gtest-1.6.0/src/gtest-all.cc gtest-1.6.0/src/gtest_main.cc
+ar -rv libgtest.a gtest-all.o gtest_main.o
+rm -rf gtest-all.o gtest_main.o
+[[ -e "../lib/" ]] || mkdir ../lib/
+mv libgtest.a ../lib/
 

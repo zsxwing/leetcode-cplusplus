9633a43ad1631c545d16f672b7344a800da2a730
zsxwing <zsxwing@gmail.com>
2013-10-30 23:36:22 +0800

    Solve the 'Palindrome Partitioning II' question

---
+++ src/palindrome_partitioning_ii.h
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  int minCut(string s) {
+    if (s.empty()) {
+      return 0;
+    }
+    vector<vector<bool>> isPalindrome(s.size(), vector<bool>(s.size(), false));
+    vector<int> dp(s.size() + 1, -1);
+    for (int i = 1; i <= s.size(); i++) {
+      dp[i] = i - 1;
+    }
+    for (int i = 0; i < s.size(); i++) {
+      for (int j = i; j >= 0; j--) {
+        if (s[i] == s[j] && (i - j < 2 || isPalindrome[j + 1][i - 1])) {
+          isPalindrome[j][i] = true;
+          dp[i + 1] = min(dp[i + 1], dp[j] + 1);
+        }
+      }
+    }
+    return dp[s.size()];
+  }
+};

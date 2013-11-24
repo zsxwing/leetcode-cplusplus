8952a7c845e54a40a9ee7fda502c0594aa937091
zsxwing <zsxwing@gmail.com>
2013-11-24 19:01:57 +0800

    Solve the 'Longest Common Prefix' question

--- src/longest_common_prefix.h
+++ src/longest_common_prefix.h
@@ -1,23 +1,17 @@
-#include <string>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
   string longestCommonPrefix(vector<string> &strs) {
     if (strs.size() == 0) {
       return "";
     }
-    string commonPrefix;
     for (int i = 0; i < strs[0].size(); i++) {
       char c = strs[0][i];
       for (int j = 1; j < strs.size(); j++) {
         if (strs[j].size() <= i || c != strs[j][i]) {
-          return commonPrefix;
+          return strs[0].substr(0, i);
         }
       }
-      commonPrefix += c;
     }
-    return commonPrefix;
+    return strs[0];
   }
 };
